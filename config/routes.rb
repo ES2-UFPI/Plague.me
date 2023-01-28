dcf774ff1565a9c297e95578174fb3e2712cfe45
thiagoexe <thiagobriitto@outlook.com>
2023-01-28 21:40:01 +0000

    Criados desenvolvedoras, generos, plataformas, publicadoras e a pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  resources :platforms
+  resources :developers
+  resources :publishers
+  resources :genres
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
