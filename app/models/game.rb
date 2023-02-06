35b38fb4e222e7c87e17d5b7f9d3cc13b774de14
Gabriel Dantas <gabrieldantas2000@ufpi.edu.br>
2023-02-06 14:44:18 +0000

    Correção Model Games e Relacionamentos

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,10 @@
 class Game < ApplicationRecord
     belongs_to :publisher
-    has_and_belongs_to_many :genres, join_table: :game_genres#, through: :game_genres
+    has_and_belongs_to_many :genres, join_table: :game_genres
     accepts_nested_attributes_for :genres
     has_many :promotion_games
     has_many :promotions, through: :promotion_games
-    has_many :game_genres
-    has_many :genres, through: :game_genres
+    has_and_belongs_to_many :platforms, join_table: :game_platforms
     accepts_nested_attributes_for :platforms
-    has_and_belongs_to_many :platforms, join_table: :game_platforms#, through: :game_platforms
     belongs_to :developer
 end
