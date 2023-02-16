aa4155b5f08f9e7cdd99cec6fc2fc8a66d7067dc
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-02-16 11:39:30 -0300

    Criando teste para game

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save game without name" do
+    game = Game.new
+    assert_not game.save, "Saved the game without a name"
+  end
+
+  test "should save game with all attributes" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: Date.today,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Could not save the game with all attributes"
+  end
 end
