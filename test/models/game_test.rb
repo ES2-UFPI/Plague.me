52737a93d41add76f9358ae8b29cfe1374dd1d6d
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-02-23 10:01:14 -0300

    Criando teste para jogos

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -2,6 +2,36 @@ require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
 
+  test "should save game with all attributes" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: Date.today,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Could not save the game with all attributes"
+  end
+
+  test 'should not save game without a title' do
+    game = Game.new
+    assert_not game.save, 'Saved the game without a title'
+  end
+
+  test "should save game without optional attributes" do
+    game = Game.new(
+      name_game: "Test Game", 
+      release: Date.today, 
+      publisher_id: 1, 
+      developer_id: 1
+    )
+    assert game.save
+  end
+
   test "should not save game without a publisher" do
     game = Game.new(
       name_game: 'Example Game',
