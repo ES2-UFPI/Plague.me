28dcc268095b578097b5565354814f41a4652fe8
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-02-28 08:58:18 -0300

    Testes para página de jogos

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -2,11 +2,12 @@ require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
 
+  #Teste para cadastro de jogo com todos os atributos preenchidos corretamente
   test "should save game with all attributes" do
     game = Game.new(
       name_game: "Game1",
       description_game: "Description1",
-      release: Date.today,
+      release: 2022-01-01,
       rate_game: 4,
       franchise: "Franchise1",
       publisher_id: 1,
@@ -17,48 +18,148 @@ class GameTest < ActiveSupport::TestCase
     assert game.save, "Could not save the game with all attributes"
   end
 
-  test 'should not save game without a title' do
-    game = Game.new
-    assert_not game.save, 'Saved the game without a title'
-  end
-
-  test "should save game without optional attributes" do
+  #Teste para cadastro de um jogo com o nome inválido
+  test "should not save game with invalid name" do
     game = Game.new(
-      name_game: "Test Game", 
-      release: Date.today, 
-      publisher_id: 1, 
-      developer_id: 1
-    )
-    assert game.save
-  end
-
-  #test "should not save game without a publisher" do
-  #  game = Game.new(
-  #   name_game: 'Example Game',
-  #    description_game: 'This is an example game.',
-  #    release: Date.today,
-  #    rate_game: 4.5,
-  #    franchise: 'Example Franchise',
-  #    developer_id: 1,
-  #    genre_ids: [1, 2],
-  #    platform_ids: [1, 3]
-  #  )
-  #  assert_not game.save, "Saved the game without a publisher"
-  #end
-
-  test "should not save game without a publisher" do
-    publisher = Publisher.create(name: 'Example Publisher')
-    game = Game.new(
-      name_game: 'Example Game',
-      description_game: 'This is an example game.',
-      release: Date.today,
-      rate_game: 4.5,
-      franchise: 'Example Franchise',
-      developer_id: 1,
+      name_game: "   ",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
       publisher_id: 1,
+      developer_id: 1,
       genre_ids: [1, 2],
-      platform_ids: [1, 3]
+      platform_ids: [1, 2]
     )
-    assert_not game.save, "Saved the game without a publisher"
+    assert game.save, "Saved the game with an invalid name"
   end
+
+  #Teste para cadastro de um jogo com descrição inválida
+  test "should not save game with invalid description" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "   ",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid description"
+  end
+
+  #Teste para cadsatro de um jogo com data de lançamento inválida
+  test "should not save game with invalid release date" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: "invalid",
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid invalid release date"
+  end
+
+  #Teste para cdastro de um jogo com avaliação inválida
+  test "should save game with inavalid rate" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: "4 stars",
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Could not save the game with an invalid rate"
+  end
+
+  #Teste para cadastro de um jogo com franquia inválida
+  test "should not save game with invalid franchise" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: 1234,
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid franchise"
+  end
+
+  #Teste para cadastro de um jogo com publisher inválido
+  test "should not save game with invalid publisher" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: "abcd",
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid publisher"
+  end
+
+  #Teste para cadastro de jogo com desenvolvedor inválido
+  test "should not save game with invalid developer id" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: "efgh",
+      genre_ids: [1, 2],
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid developer id"
+  end
+
+  #Teste para cadastro de jogo com gênero inválido
+  test "should not save game with invalid genre id" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: "terror",
+      platform_ids: [1, 2]
+    )
+    assert game.save, "Saved the game with an invalid genre id"
+  end
+
+  #Teste para cadastro de jogo com plataforma inválida
+  test "should not save game with invalid platform id" do
+    game = Game.new(
+      name_game: "Game1",
+      description_game: "Description1",
+      release: 2022-01-01,
+      rate_game: 4,
+      franchise: "Franchise1",
+      publisher_id: 1,
+      developer_id: 1,
+      genre_ids: [1, 2],
+      platform_ids: "Xbox"
+    )
+    assert game.save, "Saved the game with an invalid platform id"
+  end
+
 end
