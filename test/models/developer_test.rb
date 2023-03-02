bc43c3f9dc0eef63f7df2abce9d314bc269d9af1
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-03-02 01:35:33 -0300

    Teste para página de developers

--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -1,7 +1,32 @@
-require "test_helper"
+require 'test_helper'
 
 class DeveloperTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #Teste para cadastro de developers com todos os atributos
+  test "should save developer with all attributes" do
+    developer = Developer.new(
+      name_developer: "Developer1", 
+      description_developer: "Description developer"
+    )
+    assert developer.save, "Could not save the developer with all atributes"
+  end
+
+  #Teste para cadastro de developers com nome inválido
+  test "should save developer with an invalid name" do
+    developer = Developer.new(
+      name_developer: "   ", 
+      description_developer: "Description developer"
+    )
+    refute developer.save, "Saved the developer with an invalid name"
+  end
+
+  #Teste para cadastro de developers com uma descrição inválida
+  test "should save developer with an invalid description" do
+    developer = Developer.new(
+      name_developer: "Developer1", 
+      description_developer: "   "
+    )
+    refute developer.save, "Saved the developer with an invalid description"
+  end
+
 end
