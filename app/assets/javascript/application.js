43eb93996cd6aada7f790b0832f5ea7a7597fe55
thiagoexe <thiagobriitto@outlook.com>
2023-03-16 12:24:31 +0000

    Front pagina inicial implementado #28

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -4,3 +4,27 @@ import "controllers"
 import "jquery"
 import "@popperjs/core"
 import "bootstrap"
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: "#mainNav",
+        offset: 74
+      });
+    }
+  
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(
+      document.querySelectorAll("#navbarResponsive .nav-link")
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener("click", () => {
+        if (window.getComputedStyle(navbarToggler).display !== "none") {
+          navbarToggler.click();
+        }
+      });
+    });
+  });
