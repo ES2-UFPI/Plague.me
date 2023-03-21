7f7480f68235dc2cd892a79b6fc9608e64205d7f
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-03-21 11:39:27 -0300

    Testes para associação de platforms com game on issue #29

--- test/models/game_platform_test.rb
+++ test/models/game_platform_test.rb
@@ -5,7 +5,7 @@ class GamePlatformTest < ActiveSupport::TestCase
   #Teste para verificar se um game_platform é salvo sem um game ou platform
   test "should not save game_platform without game or platform" do
     game_platform = GamePlatform.new
-    assert_not game_platform.save, "Saved the game_platform without game or platform"
+    refute game_platform.save, "Saved the game_platform without game or platform"
   end
 
   #Teste para verificar se um game_platform é salvo com game e platform
@@ -26,8 +26,9 @@ class GamePlatformTest < ActiveSupport::TestCase
       platform_id: platform.id)
     assert game_platform.save, "Could not save the game_platform with game and platform"
   end
-  #Teste para saber se umaplataforma está associada a um game 
-  test "should associate platform to game" do
+
+  #Teste para saber se uma plataforma está associada a um game 
+  test "should associate platform to a game" do
     genre1 = Genre.create(name_genre: "Genre1", description_genre: "Genre1")
     platform1 = Platform.create(name_platform: "Platform1", description_platform: "Platform1")
     game = Game.create(
@@ -38,11 +39,10 @@ class GamePlatformTest < ActiveSupport::TestCase
     franchise: "Franchise1",
     publisher_id: 1,
     developer_id: 1,
-    genre_ids: [genre1.id]
+    genre_ids: [genre1.id],
+    platform_ids: []
     )
-    assert_difference('GamePlatform.count') do
-    game_platform = GamePlatform.create(game_id: game.id, platform_id: platform1.id)
-    assert game_platform.persisted?, "Failed to associate platform to game"
-    end
-      
+    assert game.platforms << platform1, "Could not associate platform to game"
+  end
+
 end
