93babf11536eba0a4b42624d74391723553a2cf4
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-03-21 10:05:09 -0300

    Testando assciação de platform com game on issue #29

--- test/models/game_platform_test.rb
+++ test/models/game_platform_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class GamePlatformTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #Teste para verificar se um game_platform é salvo sem um game ou platform
+  test "should not save game_platform without game or platform" do
+    game_platform = GamePlatform.new
+    assert_not game_platform.save, "Saved the game_platform without game or platform"
+  end
+
+  #Teste para verificar se um game_platform é salvo com game e platform
+  test "should save game_platform with game and platform" do
+    game = Game.create(
+      name_game: "Game1", 
+      description_game: "Description1", 
+      release: 2022-01-01, 
+      rate_game: 4, 
+      franchise: "Franchise1", 
+      publisher_id: 1, 
+      developer_id: 1)
+    platform = Platform.create(
+      name_platform: "Platform1", 
+      description_platform: "Description1")
+    game_platform = GamePlatform.new(
+      game_id: game.id, 
+      platform_id: platform.id)
+    assert game_platform.save, "Could not save the game_platform with game and platform"
+  end
+  #Teste para saber se umaplataforma está associada a um game 
+  test "should associate platform to game" do
+    genre1 = Genre.create(name_genre: "Genre1", description_genre: "Genre1")
+    platform1 = Platform.create(name_platform: "Platform1", description_platform: "Platform1")
+    game = Game.create(
+    name_game: "Game1",
+    description_game: "Description1",
+    release: 2022-01-01,
+    rate_game: 4,
+    franchise: "Franchise1",
+    publisher_id: 1,
+    developer_id: 1,
+    genre_ids: [genre1.id]
+    )
+    assert_difference('GamePlatform.count') do
+    game_platform = GamePlatform.create(game_id: game.id, platform_id: platform1.id)
+    assert game_platform.persisted?, "Failed to associate platform to game"
+    end
+      
 end
