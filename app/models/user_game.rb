9172b68a9d7e278e300c404b7d396fd456d78925
Gabriel Dantas <gabrieldantas2000@ufpi.edu.br>
2023-03-25 01:19:34 +0000

    Improve

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,6 +1,7 @@
 class UserGame < ApplicationRecord
   validates :user, :game, presence: true
+  validates :game_id, uniqueness: { scope: :user_id }
   belongs_to :user
   belongs_to :game
-  enum status: [:on_going, :played]
+  enum status: [:played, :playing, :unfinished]
 end
