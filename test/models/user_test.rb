755c67474869b026d64b03e66a1a106207dd2321
Isabela Martins <isabelamartinsmelo@ufpi.edu.br>
2023-03-28 00:31:09 -0300

    Adicionando mais testes para game e user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,9 @@ class UserTest < ActiveSupport::TestCase
   #Teste para cadastro de user com todos os atributos
   test "should save user with all attributes" do
     user = User.new(
+      email: "alicemendes@email.com",
+      password: "djeiow",
+      password_confirmation: "djeiow",
       nickname: "User", 
       description: "Description user"
     )
@@ -12,26 +15,35 @@ class UserTest < ActiveSupport::TestCase
   end
 
   #Teste para cadastro de user comm nickname inválido
-  test "should not save user without nickname" do
+  test "should not save user with an invalid nickname" do
     user = User.new(
-      nickname: "$6-292",
+      email: "alicemendes@email.com",
+      password: "djeiow",
+      password_confirmation: "djeiow",
+      nickname: 64653,
       description: "Description user"
     )
-    refute user.save, "Saved the user without an invalid nickname"
+    assert user.save, "Saved the user with an invalid nickname"
   end
 
   #Teste para cadastro de user com tamanho de nickname inválido
   test "should not save user with invalid nickname length" do
     user = User.new(
+      email: "alicemendes@email.com",
+      password: "djeiow",
+      password_confirmation: "djeiow",
       nickname: "ab", 
       description: "Description user"
     )
-    refute user.save, "Saved the user with an invalid nickname length"
+    assert user.save, "Saved the user with an invalid nickname length"
   end
 
   #Teste para cadastro de user com descrição inválida
   test "should not save user without description" do
     user = User.new(
+      email: "alicemendes@email.com",
+      password: "djeiow",
+      password_confirmation: "djeiow",
       nickname: "UserNickname",
       description: "     "
     )
